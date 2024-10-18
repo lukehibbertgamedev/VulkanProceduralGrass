4c2ae189878ea016661ad0d3a69ea0c7f2dcb523
Luke Hibbert <lukehibbertgamedev@gmail.com>
2024-10-18 17:04:29 +0100

    Some more work on the base code for a grass blade.

--- include/GrassBlade.h
+++ include/GrassBlade.h
@@ -3,15 +3,42 @@
 #include <glm/glm.hpp>  // For glm::vec2 and glm::vec3
 
 
+
 // A class representing ONE blade of grass using the Bézier representation.
 
 // Predefined blade of grass, subdivided in a tessellation shader.
 // Additional detail can be generated without needing to store it explicitly
 
+
+// 1. Generate a cubic Bézier curve using 3 control points.
+// 2. Generate a blade shape (typically a quad) that will be subdivided in the tessellation stage.
+// 3. Map/align the base blade quad to the shape of the Bézier curve by evaluating the curve interpolation of the control points for each vertex (see De Casteljau's algorithm).
+// 4. One grass blade generated???
+
+
 // https://dl.acm.org/doi/pdf/10.1145/3023368.3023380
 // Initially, the blade geometry is a flat quad that is defined by the interpolation parameters u and v, 
 // where u indicates the interpolation along the width of the blade and v the interpolation along the
-// height.By evaluating the curve interpolation of the control points for each generated vertex, the 
+// height. 
+// 
+// width = 5
+// height = 6
+// 
+// u = lerp(width_start_point, width_end_point, interpolation)
+// v = lerp(height_start_point, height_end_point, interpolation)
+// 
+// eg if interpolation = 0.5 then width = 2.5 height = 3.
+// 
+//   - - - - - 
+//   | \     |
+//   |  \    |
+//   |   \   | 
+//   |    \  |
+//   |     \ |
+//   |      \|
+//   - - - - - 
+// 
+// By evaluating the curve interpolation of the control points for each generated vertex, the 
 // quad becomes aligned to the Bezier curve. This is achieved by using De Casteljau’s algorithm, 
 // which also calculates the tangent vector t0 as intermediate results.
 
@@ -56,6 +83,38 @@
 // 	float3 b = lerp(p1, p2, t);
 // 	return lerp(a, b, t);
 // }
+
+
+
+
+namespace bezier {
+
+	const uint8_t kNumPointsAlongBezier = 10;
+
+	struct bezier { 
+	public: 
+		glm::vec3 p0, p1, p2; // Control points where p0 is the base, p1 is the height, p2 is the tip.
+		float interpolation; // Linear interpolation value between 0-1. If this = 0.5 then it is halfway between point A and point B.
+	};
+
+	// Given the coordinates of control points Pi, the first control point has coordinates P1 = (x1, y1), the second, P2 = (x2, y2) and so on.
+	// The curve coordinates are described by the equation that depends on parameter t between 0-1.
+	// This will generate a given point on the *final* bezier curve using three control points.
+	// p0, p1, and p2 are control points. interpolationValue is usually known as 't' within a lerp, the amount of positional blend between some point A and point B.
+
+	// Using De Casteljau's algorithm, which recursively evaluates polynomials in Bézier curves.
+
+	glm::vec3 lerp(const glm::vec3& p0, const glm::vec3& p1, float t) {
+		return glm::vec3((1 - t) * p0.x + t * p1.x, (1 - t) * p0.y + t * p1.y, (1 - t) * p0.z + t * p1.z);
+	}
+
+	glm::vec3 deCasteljau(const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2, float t) {
+		glm::vec3 p0p1 = lerp(p0, p1, t); 
+		glm::vec3 p1p2 = lerp(p1, p2, t); 
+		return lerp(p0p1, p1p2, t);
+	}
+}
+
 
 class Blade {
 public:
