b7081d1a750a911d25b3547df61b0eb2011031ad
Luke Hibbert <lukehibbertgamedev@gmail.com>
2024-11-27 15:15:07 +0000

    Blue clear colour for the sky, how did I forget that.

--- src/VulkanApplication.cpp
+++ src/VulkanApplication.cpp
@@ -1793,7 +1793,7 @@ void VulkanApplication::recordCommandBuffer(VkCommandBuffer commandBuffer, uint3
 
     std::array<VkClearValue, 2> clearValues = {};
     clearValues[0] = {};
-    clearValues[0].color = { {0.0f, 0.0f, 0.0f, 1.0f} };
+    clearValues[0].color = { { 0.05f, 0.3f, 0.9f, 1.0f} };
     clearValues[1] = {};
     clearValues[1].depthStencil = { 1.0f, 0 };
 
